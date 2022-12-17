c9d07b848515baf1ade56c70cd4b32cd559634a0
Darnell <curefordeception@gmail.com>
2022-12-17 12:57:22 -0800

    Week 2 Workshop

--- app.js
+++ app.js
@@ -4,12 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const campsiteRouter = require('./routes/campsiteRouter');
-const promotionRouter = require('./routes/promotionRouter');
-const partnerRouter = require('./routes/partnerRouter');
-
 const mongoose = require('mongoose');
 
 const url = 'mongodb://localhost:27017/nucampsite';
@@ -23,6 +17,12 @@ const connect = mongoose.connect(url, {
 connect.then(() => console.log('Connected correctly to server'), 
     err => console.log(err)
 );
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
 
 var app = express();
 
